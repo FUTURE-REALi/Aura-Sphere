16cb1108ad7b4d63b67b7886fd2a4917b7212494
Adarsh Pandey <adarshkhushboo3@gmail.com>
2024-10-30 22:38:01 +0530

    Middleware Update

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -9,7 +9,7 @@ const protect = (req, res, next) => {
     try {
         // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // Attach user data to the request
+        req.user = decoded; // Attach user data to the reques
         next(); // Proceed to the next middleware or route handler
     } catch (error) {
         res.status(401).json({ message: 'Token is not valid' });
